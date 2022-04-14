41bfd0895a4afdc74dfde596d03d6921b53bf6ed
davidolima <dipias2002@gmail.com>
2022-04-14 13:28:38 -0300

    1.2

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,45 +1,20 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+;; ===================================================
+;;
+;;  Configurações Doom Emacs - David de Oliveira Lima
+;;  Última atualização: 14 / 04 / 2022
+;;
+;; ===================================================
 
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-(setq user-full-name "David Lima"
+;; =========================
+;;           Geral
+;; =========================
+
+(setq user-full-name    "David Lima"
       user-mail-address "dipias2002@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-dark+)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-;;(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
 ;; Funcionalidade
@@ -60,7 +35,7 @@
 
 ;; Aparencia
 (setq doom-theme 'doom-dark+)
-(setq doom-font (font-spec :family "Hack" :size 15 :weight 'regular))
+(setq doom-font (font-spec :family "Hack Nerd Font" :size 15 :weight 'regular))
 (setq display-line-numbers-type t)
 (doom/set-frame-opacity 98)
 (tool-bar-mode 0)
@@ -70,16 +45,19 @@
 (setq-default cursor-type 'bar)
 
 
-;; Atalhos de Teclado / Keybindings
+;; ==================================
+;;  Atalhos de Teclado / Keybindings
+;; ==================================
+
+;; Geral
 (global-set-key (kbd "C-;") 'comment-region)
 (global-set-key (kbd "<f1><f2>") 'compile)
 (global-set-key (kbd "<f2><f1>") 'kill-compilation)
 (global-set-key (kbd "C-+") 'text-scale-increase)
-(global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-=") 'text-scale-decrease)
+(global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-d") 'treemacs)
 (global-set-key (kbd "C-M-<down>")  'duplicate-line)
-
 
 ;; Magit
 (global-set-key (kbd "C-<m> g") 'magit)
@@ -87,41 +65,17 @@
 (global-set-key (kbd "C-<m> c") 'magit-commit)
 (global-set-key (kbd "C-<m> p") 'magit-push)
 
+;; Outros
+(global-set-key (kbd "C-d") 'treemacs)
+(global-set-key (kbd "C-,") 'lsp-ui-imenu)
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
 
-;;; multiple cursors
+
+;; =========================
+;;         Pacotes
+;; =========================
+
+;; multiple-cursors
 (require 'multiple-cursors)
 
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
@@ -132,15 +86,20 @@
 (global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
 
 ;; lsp-mode
+(require 'lsp)
+
 (setq gc-cons-threshold 100000000)
+
 (use-package! lsp-mode
   :config
   (add-hook 'c++-mode-hook #'lsp)
   (setq lsp-clients-clangd-args '("-j=4" "-background-index" "-log=error"))
 
   (add-hook 'python-mode-hook #'lsp)
-  (add-hook 'rust-mode-hook #'lsp))
+  (add-hook 'rust-mode-hook #'lsp)
+  (add-hook 'csharp-mode-hook #'lsp))
 
+;; simpc
 (add-hook 'simpc-mode-hook
            (lambda ()
 	     (unless (file-exists-p "Makefile")
@@ -154,3 +113,6 @@
                       file
                               (file-name-sans-extension file)
                               (file-name-sans-extension file)))))))
+
+(provide 'config)
+;;; config.el ends here
